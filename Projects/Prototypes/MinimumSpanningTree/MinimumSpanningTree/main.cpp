a34ab833634bd46488d4538e6a0bbfaaedf6cc27
josephbarber96 <Joseph_Barber@Hotmail.co.uk>
2017-09-29 11:07:46 +0100

    Wrote new blog entry talking about new paper. Added OpenGL libraries to the repository. Organised OngoingBibliography.txt

---
+++ Projects/Prototypes/MinimumSpanningTree/MinimumSpanningTree/main.cpp
@@ -0,0 +1,38 @@
+#include <glut.h>
+
+#define WIN_SIZE 500
+
+void Display()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glutSwapBuffers();
+	glutPostRedisplay();
+}
+
+void InitGL()
+{
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+	// Matrix mode
+	glMatrixMode(GL_PROJECTION);
+
+	// Setup coordinate system
+	gluOrtho2D(0, WIN_SIZE, 0, WIN_SIZE);
+}
+
+int main(int argc, char* argv[])
+{
+	// Init OpenGL
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+	glutInitWindowSize(WIN_SIZE, WIN_SIZE);
+	glutCreateWindow("Minimum spanning tree.");
+	InitGL();
+
+	// Display
+	glutDisplayFunc(Display);
+	glutMainLoop();
+
+	return 0;
+}
